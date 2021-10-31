ac05320e40a42952765ff1e55f737ca71be8679b
nathalycossio <deboracossio@gmail.com>
2021-10-31 19:27:23 -0300

    modificaded json and can use at home with ejs and foreach

--- proyectoG7/src/controllers/main.js
+++ proyectoG7/src/controllers/main.js
@@ -8,12 +8,13 @@ const fs = require('fs');
 const productsFilePath = path.join(__dirname, '../data/products.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
+const productList = require('../data/products.json'); 
 // objeto literal con las acciones para cada ruta
 // cada propiedad de un objeto va a ser una respuesta a una ruta (no sabemos cual eso depende del enrutador)
 const mainController = {
     //controlador para la ruta index
     index: (req,res) => {
-        res.render('index');
+        res.render('index', {productList:productList} );
     },
     login: (req,res) => {
         res.render('login');
